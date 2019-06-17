623e62e8ebea99dc4879777a8b8ae25cfd735b0b
Neha <nehakk.2014@gmail.com>
2019-06-17 06:20:05 -0700

    Add files via upload

---
+++ creating_dictionary.py
@@ -0,0 +1,10 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> rhcproles = { "Anthony Kiedis" : "Lead Vocalist", "Chad Smith
+	      
+SyntaxError: EOL while scanning string literal
+>>> rhcproles = {"Anthony Kiedis
+	     
+SyntaxError: EOL while scanning string literal
+>>> rhcproles = {"Anthony" : "Lead Vocalist", "Chad" : "Drums", "Flea" : "Bass", "John" : "Lead Guitar"}
+>>> 
